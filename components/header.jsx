b3e3be6d7053c8904d34d51970307d278a46c759
LizzieH97 <erhughes97@gmail.com#>
2024-11-08 22:48:26 +0000

    hosting on netlify

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="header-grid">
       <h1 className="header-title">Welcome to Blobby Town</h1>
-      <img src="public/assets/download.jpeg" className="blobby-header" />
+      <img src="assets/download.jpeg" className="blobby-header" />
       <h2 className="header-subtitle">May God have mercy on your soul</h2>
     </div>
   );
