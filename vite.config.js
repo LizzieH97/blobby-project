a44ac0d6fd64662e89408a325d6379595084229d
LizzieH97 <erhughes97@gmail.com#>
2024-11-08 22:43:13 +0000

    hosting to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 4096, // This increases the size limit for assets like images to be inlined
+  },
+
   plugins: [react()],
-})
+});
